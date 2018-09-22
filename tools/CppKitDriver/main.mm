4c9d62ebeb60003af3b7c4c36073197de9d76d95
Gyula Gubacsi <gyula.gubacsi@gmail.com>
2018-09-22 21:30:48 +0100

    Starter project for Cocoa environment on MacOS

---
+++ tools/CppKitDriver/main.mm
@@ -0,0 +1,25 @@
+#include <Cocoa/Cocoa.h>
+
+int main(int argc, const char** argv) {
+  [NSAutoreleasePool new];
+  [NSApplication sharedApplication];
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+  id window = [[[NSWindow alloc]
+      initWithContentRect: NSMakeRect(0, 0, 800, 600)
+      styleMask: NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable 
+      backing:NSBackingStoreBuffered
+      defer:NO
+    ]
+    autorelease
+  ];
+
+  [window cascadeTopLeftFromPoint:NSMakePoint(20, 20)];
+  [window setTitle: @"Hello, World!"];
+  [window makeKeyAndOrderFront:nil];
+
+  [NSApp activateIgnoringOtherApps:YES];
+  [NSApp run];
+
+  return 0;
+}
